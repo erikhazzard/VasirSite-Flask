6fa207aa1ff4b665904a0837220a471e4f6ff3a9
Erik Hazzard <enoex1@gmail.com>
2012-07-20 15:34:13 -0700

    initial commit, setting up structure for app

---
+++ app.py
@@ -0,0 +1,76 @@
+# ==============================================================================
+# app.py
+#   A backend example for the proto demo.  Will serve up data and static pages
+#
+# ==============================================================================
+import flask
+import pymongo 
+import datetime
+import json
+import re
+from backend import settings
+
+# ==============================================================================
+#
+# Setup flask app
+#
+# ==============================================================================
+app = flask.Flask(__name__)
+DB_CONNECTION = pymongo.Connection('localhost', 27017)
+DB = DB_CONNECTION.vasir_site
+
+#Settings from config
+PORT = getattr(settings, 'PORT', 8080)
+ENV = getattr(settings, 'ENV', 'develop')
+
+# ==============================================================================
+#
+# Static Endpoints
+#
+# ==============================================================================
+def render_skeleton(template_name='index.html', **kwargs):
+    '''Base render func, everything get passed through here
+    '''
+    return flask.render_template(template_name, **kwargs)
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+# ==============================================================================
+#
+# Posts
+#
+# ==============================================================================
+@app.route('/items/')
+@app.route('/items/<query>/')
+def items(query=None):
+    '''Gets items from mongo based on query
+    '''
+    #Get the items from the database based on the query (if any)
+    db_items = get_items_from_query(query)
+
+    items = [] 
+    for item in db_items:
+        #Remove the _id since it's returned as an ObjectID object
+        del(item['_id'])
+        items.append(item)
+
+    #Get the total length
+    num_results = len(items)
+    
+    res = {
+        "models": items, 
+        "num_results": num_results
+    }
+    return flask.jsonify(res)
+
+# ==============================================================================
+#
+# Run server
+#
+# ==============================================================================
+if __name__ == "__main__":
+    if ENV == 'develop':
+        app.debug = True
+    app.run(port=PORT)
---
+++ backend/settings.py
@@ -0,0 +1,2 @@
+ENV= getattr(local_settings, 'ENV', 'develop')
+PORT = getattr(local_settings, 'PORT', 8080)
