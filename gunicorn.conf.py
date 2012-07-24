b469e402e5103484a18fe41a00ea16a2510f8440
Erik Hazzard <erik@ip-10-28-114-59.ec2.internal>
2012-07-24 07:30:34 +0000

    updated upstart files, gunicorn scripts

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,5 @@
 bind = "127.0.0.1:8000"
 logfile = "/var/log/gunicorn/vasirsite.log"
+pid="/var/run/gunicorn_vasirsite.pid"
 workers=3
 worker_class='gevent'
--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #Run gunicorn
-gunicorn app:app -c /home/erik/Code/VasirSite-Flask/gunicorn.conf.py
+PID_FILE=/var/run/gunicorn_vasirsite.pid
+. env/bin/activate
+gunicorn app:app -c /home/erik/Code/VasirSite-Flask/gunicorn.conf.py --pid=$PID_FILE
