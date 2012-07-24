1624b179c9b165bca569123536b02d4500d9aa28
Erik - Server <enoex1@gmail.com>
2012-07-24 07:51:24 +0000

    fixes deploy scripts, gunicorn scripts

--- deploy/run_gunicorn.sh
+++ deploy/run_gunicorn.sh
@@ -2,4 +2,5 @@
 #Run gunicorn
 PID_FILE=/var/run/gunicorn_vasirsite.pid
 . env/bin/activate
+cd /home/erik/Code/VaisrSite-Flask/
 gunicorn app:app -c /home/erik/Code/VasirSite-Flask/deploy/gunicorn.conf.py --pid=$PID_FILE
