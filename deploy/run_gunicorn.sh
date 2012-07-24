4e36cd5950ca579b8549ae11be6ad2bae16c1fa9
Erik - Server <enoex1@gmail.com>
2012-07-24 07:40:51 +0000

    updated upstart / gunicorn scripts

---
+++ deploy/run_gunicorn.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Run gunicorn
+PID_FILE=/var/run/gunicorn_vasirsite.pid
+. env/bin/activate
+gunicorn app:app -c /home/erik/Code/VasirSite-Flask/deploy/gunicorn.conf.py --pid=$PID_FILE
--- run_gunicorn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Run gunicorn
-PID_FILE=/var/run/gunicorn_vasirsite.pid
-. env/bin/activate
-gunicorn app:app -c /home/erik/Code/VasirSite-Flask/gunicorn.conf.py --pid=$PID_FILE
